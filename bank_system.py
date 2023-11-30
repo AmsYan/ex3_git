6d9bb9cec139c47398419634f0357043860f7e5c
YanivAms <amsellem.yaniv@gmail.com>
2023-11-30 11:12:39 +0200

    third commit

--- bank_system.py
+++ bank_system.py
@@ -1,7 +1,31 @@
+import random
+
+
 def create_account():
-    return
+    account_id = random.randint(0, 999)
+    balance = random.randint(0, 99999)
+    return (account_id, balance)
+
+
+def check_balance(client):
+    return client[1]
+
+
+def balance_inquiry():
     pass
 
 
-def check_balance(account_id):
+def deposit(d, amount):
     pass
+
+
+def withdraw(w, amount):
+    pass
+
+
+def account_summary(account_id):
+    pass
+
+
+person1 = create_account()
+print(check_balance(person1))
