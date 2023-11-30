264af8496b698225e78841b5038ecbdcae49c88c
YanivAms <amsellem.yaniv@gmail.com>
2023-11-30 11:25:27 +0200

    docsting

--- bank_system.py
+++ bank_system.py
@@ -2,21 +2,28 @@ import random
 
 
 def create_account():
+    """    Create a new bank account with a randomly generated account ID and balance.
+    :return list representing them
+"""
     account_id = random.randint(0, 999)
     balance = random.randint(0, 99999)
     return [account_id, balance]
 
 
 def balance_inquiry(client):
+    """ receive client info and return the balance amount"""
     return client[1]
 
 
 def deposit(client, amount):
+    """receive client info and amunt to add return new balance"""
     client[1] = client[1] + amount
     return client[1]
 
 
 def withdraw(client, amount):
+    """receive client info and amount to withdraw check if thre is enough money
+    if yes return new balance"""
     if client[1] >= amount:
         client[1] = client[1] - amount
         return client[1]
@@ -26,6 +33,7 @@ def withdraw(client, amount):
 
 
 def account_summary(client):
+    """ receive client info and return a summary of the info"""
     print(f"The client {client[0]} have a balance of {client[1]}")
 
 
@@ -33,6 +41,6 @@ person1 = create_account()
 print(balance_inquiry(person1))
 person1[1] = deposit(person1, 1000)
 print(balance_inquiry(person1))
-withdraw(person1,10000)
+withdraw(person1, 10000)
 print(balance_inquiry(person1))
 account_summary(person1)
